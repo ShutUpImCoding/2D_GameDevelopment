c2edabfca64dcb2ef6b47f92a810a865452ea203
Netta <n2ojne00@students.oamk.fi>
2026-02-06 19:16:14 +0200

    ball directs movement to touch point

--- app/src/main/java/com/example/a2d_gamedevelopment_project/gameView/GameView.kt
+++ app/src/main/java/com/example/a2d_gamedevelopment_project/gameView/GameView.kt
@@ -4,9 +4,20 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.view.MotionEvent
 import android.view.SurfaceView
+import kotlin.math.sqrt
 
 class GameView(context: Context) : SurfaceView(context), Runnable {
+
+    private var ballX = 300f
+    private var ballY = 300f
+    private val ballRadius = 50f
+
+    private val launchSpeed = 15f
+
+    private var velocityX = 0f
+    private var velocityY = 0f
 
     private var thread: Thread? = null
     private var running = false
@@ -15,17 +26,17 @@ class GameView(context: Context) : SurfaceView(context), Runnable {
     fun resume() {
         running = true
         thread = Thread(this)
-        thread!!.start()
+        thread !!.start()
     }
 
     fun pause() {
         running = false
-        thread!!.join()
+        thread !!.join()
     }
 
     override fun run() {
         while (running) {
-            if (!holder.surface.isValid) continue
+            if (! holder.surface.isValid) continue
 
             update()
 
@@ -35,13 +46,39 @@ class GameView(context: Context) : SurfaceView(context), Runnable {
         }
     }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+
+            val dx = event.x - ballX
+            val dy = event.y - ballY
+
+            val length = sqrt(dx * dx + dy * dy)
+
+            if (length != 0f) {
+                velocityX = (dx / length) * launchSpeed
+                velocityY = (dy / length) * launchSpeed
+            }
+        }
+        return true
+    }
+
     private fun update() {
-        // game logic later
+        ballX += velocityX
+        ballY += velocityY
+
+        if (ballX <= ballRadius || ballX >= width - ballRadius) {
+            velocityX *= - 1
+        }
+
+        if (ballY <= ballRadius || ballY >= height - ballRadius) {
+            velocityY *= - 1
+        }
+
     }
 
     private fun drawGame(canvas: Canvas) {
         canvas.drawColor(Color.BLACK)
-        paint.color = Color.WHITE
-        canvas.drawCircle(300f, 300f, 50f, paint)
+        paint.color = Color.MAGENTA
+        canvas.drawCircle(ballX, ballY, ballRadius, paint)
     }
 }
